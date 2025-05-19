ed0be57337c9005ae4a68b45cd77fd53da532716
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-05-19 18:32:53 +0530

    Some fixes

--- middleware/user.js
+++ middleware/user.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 function restrictToLoggedinUserOnly(req, res, next) {
   const token = req.cookies.uid;
 
-  if (!token) return res.status(401).json({ error: 'Access denied' });
+  // if (!token) return res.status(401).json({ error: 'Access denied' });
+  if (!token) return res.status(401).render("unauthorized");
 
   try {
     const verified = jwt.verify(token, 'Sarthak$123@$');

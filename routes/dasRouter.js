e1d8a9007a914dd591dce397f290879f5042bbd3
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-05-19 17:57:33 +0530

    Added the user at dashboard

--- routes/dasRouter.js
+++ routes/dasRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const prisma = require("../prisma/client.js");
 
-router.get("/", (req, res) => {
-  res.render("dashboard");
+router.get("/", async (req, res) => {
+  const userId = req.user.id;
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+    });
+   res.render("dashboard", {user});
 });
 
 module.exports = router;

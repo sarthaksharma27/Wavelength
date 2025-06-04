bc6370ae61966914a64ea8b0297f579058a3b252
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-06-04 23:40:17 +0530

    Added roomId with user

---
+++ prisma/migrations/20250604175910_add_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "roomIds" SET DEFAULT ARRAY[]::TEXT[];
--- routes/studioRouter.js
+++ routes/studioRouter.js
@@ -4,21 +4,38 @@ const { v4: uuidv4 } = require('uuid');
 const prisma = require("../prisma/client.js");
 const restrictToLoggedinUserOnly = require('../middleware/user.js');
 
-router.get('/', (req, res) => {
+router.get('/', restrictToLoggedinUserOnly, async (req, res) => {
   const roomId = uuidv4();
+
+  try {
+    if (req.user && req.user.id) {
+      const updatedUser = await prisma.user.update({
+        where: { id: req.user.id },
+        data: {
+          roomIds: {
+            push: roomId,
+          },
+        },
+      });
+      // console.log('Updated user:', updatedUser);
+    }
+  } catch (error) {
+    console.error('Error updating user with roomId:', error);
+  }
+
   res.redirect(`/studio/${roomId}`);
 });
 
 router.get('/:roomId', restrictToLoggedinUserOnly, async (req, res) => {
   const token = req.cookies.uid;
-  if(token) { // which mean you are loged in user
+  if(token) { // logged in user
     const userId = req.user.id;
 
     const user = await prisma.user.findUnique({
         where: { id: userId },
     });
    res.render('studio/studio', { roomId: req.params.roomId, user });
-  } else { // you are a guest
+  } else { // guest
     res.render('studio/studio', { roomId: req.params.roomId });
   }
   

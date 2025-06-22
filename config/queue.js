c0152e92de7bd8b1ce4ab1bdcf6c61acb4d4f215
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-06-23 04:31:41 +0530

    Update: redis for prod

--- config/queue.js
+++ config/queue.js
@@ -4,6 +4,7 @@ const videoProcessingQueue = new Queue('video-processing', {
   redis: {
     host: process.env.REDIS_HOST ||'localhost',
     port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || '', // Needed only for production
   },
   defaultJobOptions: {
     attempts: 1, 
